f611db0ff72f8fafda7bff4a7a406dfdc8262023
arafat <arafatmoustapha281@gmail.com>
2023-01-02 20:07:04 +0100

    ajout des ressources filament

--- app/Models/Request.php
+++ app/Models/Request.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Request extends Model
 {
     use HasFactory;
+
+    protected $fillable =  [
+        'user_id',
+        'title',
+        'type',
+        'description',
+        'city',
+        'salary',
+        'preference'
+    ];
 }
